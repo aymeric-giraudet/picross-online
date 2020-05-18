697fed4a820b2f68d16e32bc4fc6419ff84a331c
Aymeric Giraudet <neadj59@gmail.com>
2020-05-18 19:37:21 +0200

    feat(PWA): setup proper PWA and better db requests (#1)
    * removed next-pwa
    
    * change links and postcss
    
    * deploy to brussels
    
    * add next-connect
    
    * add next-offline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import Header from "../components/Header";
 import "../styles/index.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
+    <Head>
+      <title>Picross Online</title>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#72B340" />
+      <meta
+        name="description"
+        content="Play Picross on any device, any time"
+      />
+    </Head>
     <Header />
     <div className="px-4">
       <div className="max-w-3xl bg-white rounded-lg mx-auto my-16 p-16">
