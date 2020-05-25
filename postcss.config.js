a4b055768743f6e86eda88cd9e0193974c5eb30e
Aymeric Giraudet <neadj59@gmail.com>
2020-05-25 18:16:04 +0200

    add body back to css whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,8 @@ const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
     content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    whitelist: ["body"],
   },
 ];
 
